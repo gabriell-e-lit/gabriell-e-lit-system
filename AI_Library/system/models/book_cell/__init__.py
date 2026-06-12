c206957e29c0ed1c79607cf96f66c04f6aa6f22f
gabriell-e-lit <gabrielazaneva@gmail.com>
2026-06-12 20:37:52 +0300

    Add files via upload

---
+++ AI_Library/system/models/book_cell/__init__.py
@@ -0,0 +1,15 @@
+from .BookCellModel_v2_0 import (
+    BookCellModel,
+    BookCellCovers,
+    BookCellFormats,
+    BookCellDisplay,
+)
+from .BookCellModel_v2_0_builder import build_book_cell_from_raw
+
+__all__ = [
+    "BookCellModel",
+    "BookCellCovers",
+    "BookCellFormats",
+    "BookCellDisplay",
+    "build_book_cell_from_raw",
+]
